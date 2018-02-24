fe5ea7987d4741c5a1005b51f8ec76dcd184578e
whitesand230 <jka2430@gmail.com>
2018-02-24 16:46:53 -0600

    Adding all files

---
+++ AccessManagementWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AccessManagementWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
